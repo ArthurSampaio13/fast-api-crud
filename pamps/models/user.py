ce7e8a6a6146008d5d0e173f13904d0bc4c6697f
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-12-30 14:38:42 -0300

    feat: model de usuario

---
+++ pamps/models/user.py
@@ -0,0 +1,10 @@
+from sqlmodel import Field, SQLModel
+from typing import Optional
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    email: str = Field(unique=True, nullable=False)
+    username: str = Field(unique=True, nullable=False)
+    avatar: Optional[str] = None
+    bio: Optional[str] = None
+    password: str = Field(nullable=False)
